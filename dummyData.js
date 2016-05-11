40644714c9a12f8848e293f130d5ca3938ad25c4
brayzen <brian.ray.biz@gmail.com>
2016-05-10 21:14:55 -0700

    added dummyData for making FAKER users

---
+++ dummyData.js
@@ -0,0 +1,44 @@
+// First run this file in node, then copy, the go to Mongo Shell:
+// var = arr;  arr.forEach(function(obj){db.customers.insert(obj)})
+
+const Faker = require('faker')
+const hoods = ['Belltown', 'Queen Anne', 'Capitol Hill']
+const interests = ['golf', 'running', 'stretching', 'cooking', 'coding', 'lifting', 'drivingRange', 'talking', 'happyHour']
+var finalArr = []
+
+function genRandNum(limit) {
+  return Math.floor(Math.random() * limit)
+}
+
+function genRandSelectArray(array, max) {
+  var copy = array.slice()
+  var randSelectArray = []
+  var randNum = genRandNum(max) + 1
+  for (var i = 0; i < randNum; i++){
+    var newRand = genRandNum(copy.length)
+    var selected = (copy[newRand] !== undefined) ? copy[newRand] : copy.last
+    randSelectArray.push(selected)
+    copy.splice(copy.indexOf(selected), 1)
+  }
+  return randSelectArray
+}
+
+var User = function(){
+  this.email      = Faker.internet.email(),
+  this.firstName  = Faker.name.firstName(),
+  this.lastName   = Faker.name.lastName(),
+  this.bio        = Faker.lorem.paragraph(2),
+  this.interests  = genRandSelectArray(interests, 5),
+  this.pic        = Faker.image.avatar(),
+  this.created_at = Faker.date.past().toString(),
+  this.rating     = genRandNum(10)
+}
+
+// NUMBER OF USERS
+
+for (var i = 1; i < 300; i++){
+  finalArr.push(new User)
+}
+
+console.log(finalArr)
+
--- models/user.js
+++ models/user.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose')
 
 var userSchema = new mongoose.Schema({
-    firstName:     String,
-    lastName:      String,
-    email:         String,
-    bio:           String,
-    interests:     Array, //max of 5
-    neighborhoods: Array, //max 2
-    pic:           String,
-    rating:        Number,
-    _favorites:    Array
+    firstName:     {type: String, required: true},
+    lastName:      {type: String},
+    email:         {type: String, required: true},
+    bio:           {type: String},
+    interests:     {type: Array, required: true}, //max of 5
+    neighborhoods: {type: Array, required: true}, //max 2
+    pic:           {type: String, required: true},
+    rating:        {type: Number},
+    _favorites:    {type: Array},
+    created_at:    {type: Date, required: true, default: Date.now }
 })
 
 module.exports = exports = mongoose.model('User', userSchema)
