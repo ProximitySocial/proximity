c149684f5e5d179592d9e4d6c96748e97eb36dd7
brayzen <brian.ray.biz@gmail.com>
2016-05-17 09:23:23 -0700

    fixed put route (dont know what is different), made googleCall into a seperate file

--- routes/event_routes.js
+++ routes/event_routes.js
@@ -3,6 +3,12 @@ const express = require('express')
 const User = require(__dirname + '/../models/user')
 const Event = require(__dirname + '/../models/event')
 const eventRouter = module.exports = exports = express.Router()
+const http = require('http')
+const callGoogle = require('../public/libs/googleLocation')
+
+// const Promise = require('promise');
+// const requestProxy  = require('express-request-proxy')
+
 
 //index of events
 eventRouter.get('/events', (req, res) => {
@@ -13,14 +19,50 @@ eventRouter.get('/events', (req, res) => {
   })
 })
 
-//create new event
-eventRouter.post('/event/new', (req, res) => {
-  var eventData = req.body
-  //add _creator from User _id
-  new Event(eventData).save((err, result) => {
-    console.log(result)
-    res.status(200).json({msg: 'event created'})
+
+//EVENTS specific to User's neighborhood
+eventRouter.get('/events/:userId', (req, res) => {
+  var userId = req.params.userId
+  var events;
+  User.find({_id: userId}, {neighborhoods: true}, (err, data) => {
+    if (err) return res.status(500).json({msg: 'Server Error'})
+    console.log(data)
+    data.forEach((hood, index) => {
+      Event.find({neighborhood: hood}, (err, arrayOfObjects) => {
+        if (err) return res.status(500).json({msg: 'Server Error'})
+        if (index == 1){
+          events.concat(arrayOfObjects)
+        } else {
+          events = arrayOfObjects
+        }
+      })
+    })
+    console.log(events)
+    res.status(200).json(events)
   })
+})
+  //create new event
+eventRouter.post('/event/new', (req, res) => {
+  //add _creator from User _id
+  var eventData = req.body
+  console.log("Made a POST request for NEW user")
+  var address = req.body.address.split(' ').join('+')
+  callGoogle(address)
+    .then((data) => {
+      eventData.neighborhood = data.results[0].address_components[2].long_name
+      eventData.locationData = data
+      console.log(eventData)
+      new Event(eventData).save((err, result) => {
+        console.log('^^^^^^^^^^^^^')
+        console.log('^^^^^^^^^^^^^')
+        console.log('^^^^^^^^^^^^^')
+        // console.log(result)
+        res.status(200).json({msg: 'event created', data: result})
+      })
+    })
+    .catch((err) => {
+      throw err;
+    })
 })
 
 //get specific event
