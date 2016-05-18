482ff37f8ea04f8b2bc5244b3cf081e8779b00d7
brayzen <brian.ray.biz@gmail.com>
2016-05-17 20:45:24 -0700

    validating arrays in user schema...max of 2 hoods and 5 interests

--- models/user.js
+++ models/user.js
@@ -1,15 +1,22 @@
 const mongoose = require('mongoose')
 
+function arrayLimit(val) {
+  return val.length <= 2;
+}
+
+function interestsLimit(val) {
+  return val.length <= 5;
+}
+
 var userSchema = new mongoose.Schema({
     firstName:     {type: String, required: true},
-    lastName:      {type: String},
+    lastName:      {type: String, minlength: 1},
     email:         {type: String, required: true},
     bio:           {type: String},
-    interests:     {type: Array}, //max of 5
-    // interests:     {type: Array, required: true}, //max of 5
-    neighborhoods: {type: Array}, //max 2
-    // neighborhoods: {type: Array, required: true}, //max 2
-    // pic:           {type: String, required: true},
+    interests:     {type: Array,
+                    validate: [interestsLimit, '{PATH} exceeds the limit of 5']}, //max of 5
+    neighborhoods: {type: Array,
+                    validate: [arrayLimit, '{PATH} exceeds the limit of 2']}, //max 2
     pic:           {type: String},
     rating:        {type: Number},
     _favorites:    {type: Array},
