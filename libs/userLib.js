64645aba2fd841e87ef0d7c7272754029416c2a4
brayzen <brian.ray.biz@gmail.com>
2016-06-02 10:49:43 -0700

    added a console.log for the user _id

--- libs/userLib.js
+++ libs/userLib.js
@@ -9,6 +9,7 @@ function createUser(userData, res){
     } else {
       new User(userData).save((err, result) => {
         if(err) {return res.status(500).json({error: err})}
+        console.log('User _id: ' + result._id)
         res.status(200).json({msg: 'user created', id: result._id, signedRequest: userData.awsData, url: userData.url})
       })
     }
